6fb5f0794357b61139695c30c220ccc485321f03
Internpurpose3dtech <intern@purpose3dtech.com>
2024-09-13 00:32:12 +0530

    second commit3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Project from "./components/Project";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
